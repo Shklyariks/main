865bb0c4f2a590f98e89e291822d6a0bec8c3b60
sergejsklarik <shklyarik.s.a@gmail.com>
2022-06-30 00:36:01 +0300

    1234

--- 1/2.py
+++ 1/2.py
@@ -1,10 +1,13 @@
 from calendar import monthrange
+import datetime
 
 def num():
     try:
         year = int(input('Введите год:\n'))
         month = int(input('Введите порядковый номер месяца: \n'))
+        date = datetime.date(1900, month, 1).strftime('%B')
         num = monthrange(year, month)[1]
+        print(date)
         print(num)
     except ValueError:
         print('Нужно ввести целое число!!!')
