2a5ef900f2c93e3e4b162823360eb9990ce15776
sergejsklarik <shklyarik.s.a@gmail.com>
2022-07-05 01:19:56 +0300

    1234

--- 2/2.1.py
+++ 2/2.1.py
@@ -1,7 +1,21 @@
 a = input().split()
 l = []
-for i in range(len(a)):
-    if i % 3 == 0 and i % 5 != 0:
+'''for i in range(len(a)):
+    if int(a[i]) % 3 == 0 and int(a[i]) % 5 != 0:
         l.append(a[i])
 
+print(l)'''
+
+for i in a:
+    if int(i) % 3 == 0 and int(i) % 5 != 0:
+        l.append(i)
 print(l)
+
+
+z = input().split()
+li = []
+for i in z:
+    if z.count(i) > 1:
+        li.append(i)
+print(li)
+
