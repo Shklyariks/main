b9d21674f1b510e178f1f592be104ed9de49f6c0
sergejsklarik <shklyarik.s.a@gmail.com>
2022-07-11 21:54:14 +0300

    1234

--- 3/3.1.py
+++ 3/3.1.py
@@ -1,8 +1,15 @@
 import time
 
 date = input()
-try:
-    time.strptime(date, '%d.%m.%Y')
-    print('OK')
-except ValueError:
-    print(f'FAILED \n {date} incorrect day')
+
+
+def time_func(date):
+    try:
+        time.strptime(date, '%d.%m.%Y')
+        print('OK')
+    except ValueError:
+        print(f'FAILED \n {date} incorrect day')
+    return date
+
+
+time_func(date)
